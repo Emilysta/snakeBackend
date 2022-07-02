d108c0ec7188faa0632ed9b27490ffeb5f46d3a1
Emilia <emiliastarczyk@gmail.com>
2022-07-02 18:30:34 +0200

    implemented endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,49 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
+import * as db from './playerScoreModel';
+import { body, validationResult, param } from 'express-validator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     return res.json({ status: 'ok' });
 });
 
-app.post('/score', (req: Request, res: Response) => {
+type PlayerScore = {
+    playerName: string,
+    playerScore: number
+}
 
-    return res.json();
-});
+app.post('/score',
+    body('playerName').notEmpty(), body('playerName').isLength({ min: 1, max: 150 }),
+    body('playerScore').isInt({ min: 0 }),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json(400).json({ errors: errors.array() });
+        }
 
-app.get('/scores/:page', (req: Request, res: Response) => {
-    return res.json({ pageNumber: req.params.page, scores: []});
+        const playerScore = req.body as PlayerScore;
+
+        const addedPlayerScore = await db.addPlayerScore(playerScore.playerName, playerScore.playerScore);
+        return res.json(addedPlayerScore);
+    });
+
+app.get('/scores/:page',
+    param('page').isInt({ min: 1 }),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json(400).json({ errors: errors.array() });
+        }
+        const pageNumber = parseInt(req.params.page);
+        const pageContent = await db.getPlayerScores(pageNumber);
+        return res.json({ pageNumber: pageNumber, scores: pageContent });
+    });
+
+app.get('/scores/pages', async (req: Request, res: Response) => {
+    const count = await db.getPagesCount();
+    return res.json({ pagesCount: count });
 });
 
 app.listen(3000, () => {
