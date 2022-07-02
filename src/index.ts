b0e5c56d9894bd111735127bb60cacd1308aafbf
Emilia <emiliastarczyk@gmail.com>
2022-07-02 16:42:09 +0200

    base for endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ status: 'ok' });
+app.get('/', (req: Request, res: Response) => {
+    return res.json({ status: 'ok' });
+});
+
+app.post('/score', (req: Request, res: Response) => {
+
+    return res.json();
+});
+
+app.get('/scores/:page', (req: Request, res: Response) => {
+    return res.json({ pageNumber: req.params.page, scores: []});
 });
 
 app.listen(3000, () => {
